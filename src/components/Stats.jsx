be3051ddb7ccb392abad71b76ae64a12dbad9313
MD. ESHTIAK AHMED <eshtiak4099@gmail.com>
2026-04-01 18:03:02 +0600

    Bug Fixing

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,14 +2,14 @@ const Stats = () => {
   return (
     <div className="bg-[#020617]">
 
-      {/* 🌈 Softer Gradient */}
+     
       <div className="w-full bg-gradient-to-r from-purple-500 to-indigo-600">
 
         <div className="px-4 py-14 sm:px-6 md:px-12">
 
           <div className="grid grid-cols-1 text-center divide-y sm:grid-cols-2 md:grid-cols-3 sm:divide-y-0 md:divide-x divide-white/20">
 
-            {/* Item 1 */}
+            {/*1 */}
             <div className="py-6 md:py-10">
               <h2 className="text-3xl font-bold text-white sm:text-4xl md:text-5xl">
                 50K+
@@ -19,7 +19,7 @@ const Stats = () => {
               </p>
             </div>
 
-            {/* Item 2 */}
+            {/* 2 */}
             <div className="py-6 md:py-10">
               <h2 className="text-3xl font-bold text-white sm:text-4xl md:text-5xl">
                 200+
@@ -29,7 +29,7 @@ const Stats = () => {
               </p>
             </div>
 
-            {/* Item 3 */}
+            {/* 3 */}
             <div className="py-6 md:py-10">
               <h2 className="text-3xl font-bold text-white sm:text-4xl md:text-5xl">
                 4.9
